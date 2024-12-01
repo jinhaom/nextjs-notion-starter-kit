d843fbf9e17ca7d399e4db4eb66c09ae505840a2
jinhaom <92512744+jinhaom@users.noreply.github.com>
2024-12-01 13:10:27 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: https://file.jinhao.site/favicon_logosc/favicon-32x32.png,
+  defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
